027a2eff2c71377a0361a079e0437b7bc605f9ae
Eduardo-95-DS <edu.yori@gmail.com>
2024-01-11 16:15:46 -0300

    updates

--- scripts/visao_geral.py
+++ scripts/visao_geral.py
@@ -94,12 +94,43 @@ def cuisines (df,col):
 
     return df2
 
-def map_(df):
+def get_country_center_coordinates(country):
+    country_coordinates = {
+        'World View' : [0, 0],
+        'United States of America': [37.0902, -95.7129],
+        'Canada': [56.1304, -106.3468],
+        'Brazil': [-14.2350, -51.9253],
+        'England': [51.5099, -0.1180],
+        'Australia': [-25.2744, 133.7751],
+        'New Zealand': [-40.9006, 174.8860],
+        'Philippines': [12.8797, 121.7740],
+        'Indonesia': [-0.7893, 113.9213],
+        'India': [20.5937, 78.9629],
+        'Sri Lanka': [7.8731, 80.7718],
+        'United Arab Emirates': [23.4241, 53.8478],
+        'Qatar': [25.2769, 51.5201],
+        'Turkey': [38.9637, 35.2433],
+        'Singapore': [1.3521, 103.8198]}
+
+    return country_coordinates.get(country, (0, 0))
     
-    st.markdown("<h1 style='text-align: center;'>World Map</h1>", unsafe_allow_html=True)
+def map_(df):
+    center_coordinates = get_country_center_coordinates(selected_country)
+    # filtered_df = df[df['country'] == selected_country]
+        
+    if selected_country == 'World View':
+        bounds = [[-90, -180], [90, 180]]
+    else:
+        bounds = None
+        
+    st.markdown("<h1 style='text-align: right;'>World Map</h1>", unsafe_allow_html=True)
 
     # Criar o mapa
-    map_ = folium.Map(control_scale=True)
+    map_ = folium.Map(location=center_coordinates,zoom_start=4, control_scale=True)
+    
+    if selected_country == 'World View':
+        map_.fit_bounds(bounds)
+        
     marker_cluster = MarkerCluster().add_to(map_)
     
     for i, row in df.iterrows():
@@ -129,14 +160,28 @@ def map_(df):
     folium_static(map_,width=1024,height=600)
 
 def cuisines_map(df):
+    center_coordinates = get_country_center_coordinates(selected_country)
     
-    st.markdown("<h1 style='text-align: center;'>Cuisines Map</h1>", unsafe_allow_html=True)
+    if selected_country == 'World View':
+        filtered_df = df  
+    else:
+        filtered_df = df[df['country'] == selected_country] 
 
+    if selected_country == 'World View':
+        bounds = [[-90, -180], [90, 180]]
+    else:
+        bounds = None
+        
+    st.markdown("<h1 style='text-align: right;'>Cuisines Map</h1>", unsafe_allow_html=True)
+        
     # Criar o mapa
-    cuisine_map = folium.Map(control_scale=True)
+    cuisine_map = folium.Map(location=center_coordinates,zoom_start=4, control_scale=True)
     marker_cluster = MarkerCluster().add_to(cuisine_map)
-    
-    for i, row in df.iterrows():
+        
+    if selected_country == 'World View':
+        cuisine_map.fit_bounds(bounds)
+        
+    for i, row in filtered_df.iterrows():
         cuisines_count = row['cuisines_n°']
         marker_size = int(row['cuisines_n°']) * 5
 
@@ -180,7 +225,7 @@ def cuisines_map(df):
                     bottom: 50px; left: 50px; width: 150px; height: 150px; 
                     border:2px solid grey; z-index:9999; font-size:14px;
                     background-color:white; opacity: .85; text-align: left; padding: 5px;">
-          <b style="font-size: 16px;">Cuisines variety:</b><br>
+          <h3 style="font-size: 16px; text-align: center;">Cuisines variety:</h3>
           <span style="color: red; font-size: 20px; vertical-align: middle;">■</span> 1 Cuisine <br>
           <span style="color: orange; font-size: 20px; vertical-align: middle;">■</span> 2-3 Cuisines <br>
           <span style="color: blue; font-size: 20px; vertical-align: middle;">■</span> 4-6 Cuisines <br>
@@ -190,19 +235,33 @@ def cuisines_map(df):
     
     cuisine_map.get_root().html.add_child(folium.Element(legend_html))
     
-    folium_static(cuisine_map,width=1024,height=600)
+    folium_static(cuisine_map, width=1024, height=600)
 
 def price_map(df):
-
-    st.markdown("<h1 style='text-align: center;'>Price Range Map</h1>", unsafe_allow_html=True)
-
-    price_map = folium.Map(control_scale=True)
-    marker_cluster = MarkerCluster().add_to(price_map)
+    center_coordinates = get_country_center_coordinates(selected_country)
     
+    if selected_country == 'World View':
+        filtered_df = df  
+    else:
+        filtered_df = df[df['country'] == selected_country] 
+
+    if selected_country == 'World View':
+        bounds = [[-90, -180], [90, 180]]
+    else:
+        bounds = None
+        
+    st.markdown("<h1 style='text-align: right;'>Price Range Map</h1>", unsafe_allow_html=True)
+
+    price_map = folium.Map(location=center_coordinates,zoom_start=4, control_scale=True)
+    marker_cluster = MarkerCluster().add_to(price_map)
+
+    if selected_country == 'World View':
+        price_map.fit_bounds(bounds)
+        
     price_color = {'gourmet': 'red', 'expensive': 'orange', 'normal': 'blue', 'cheap': 'green'}
     price_size = {'gourmet': 25, 'expensive': 20, 'normal': 15, 'cheap': 10}
-    
-    for i, row in df.iterrows():
+        
+    for i, row in filtered_df.iterrows():
         
         marker_size = price_size.get(row['price_range'], 8)  # Tamanho padrão é 8 se não estiver nas faixas especificadas
         fill_color = price_color.get(row['price_range'], 'gray')
@@ -250,16 +309,30 @@ def price_map(df):
     price_map.get_root().html.add_child(folium.Element(legend_html))
     folium_static(price_map,width=1024,height=600)
 
-def aggregate_map (df):
+def agg_map (df):
+    center_coordinates = get_country_center_coordinates(selected_country)
+    
+    if selected_country == 'World View':
+        filtered_df = df  
+    else:
+        filtered_df = df[df['country'] == selected_country] 
 
-    st.markdown("<h1 style='text-align: center;'>Aggregate Rating Map</h1>", unsafe_allow_html=True)
+    if selected_country == 'World View':
+        bounds = [[-90, -180], [90, 180]]
+    else:
+        bounds = None
+        
+    st.markdown("<h1 style='text-align: right;'>Aggregate Rating Map</h1>", unsafe_allow_html=True)
 
-    agg_map = folium.Map(control_scale=True)
+    agg_map = folium.Map(location=center_coordinates,zoom_start=4,control_scale=True)
     marker_cluster = MarkerCluster().add_to(agg_map)
     
     rating_size = {'bad': 10, 'regular': 15, 'good': 20, 'excellent': 25}
     
-    for i, row in df.iterrows():
+    if selected_country == 'World View':
+        agg_map.fit_bounds(bounds)
+        
+    for i, row in filtered_df.iterrows():
         rating = row['aggregate_rating']
     
         if rating == 0:
@@ -323,15 +396,29 @@ def aggregate_map (df):
     folium_static(agg_map,width=1024,height=600)
 
 def avg_map(df):
+    center_coordinates = get_country_center_coordinates(selected_country)
+    
+    if selected_country == 'World View':
+        filtered_df = df  
+    else:
+        filtered_df = df[df['country'] == selected_country] 
 
+    if selected_country == 'World View':
+        bounds = [[-90, -180], [90, 180]]
+    else:
+        bounds = None    
+    
     st.markdown("<h1 style='text-align: center;'>Average Cost for Two Map</h1>", unsafe_allow_html=True)
 
-    avg_map = folium.Map(control_scale=True)
+    avg_map = folium.Map(location=center_coordinates, zoom_start=4, control_scale=True)
     marker_cluster = MarkerCluster().add_to(avg_map)
     
     cost_size = {'cheap': 10, 'normal': 15, 'expensive': 20, 'gourmet': 25}
-    
-    for i, row in df.iterrows():
+
+    if selected_country == 'World View':
+        avg_map.fit_bounds(bounds)
+        
+    for i, row in filtered_df.iterrows():
         avg_cost = row['average_cost_for_two']
     
         if 0 <= avg_cost <= 20:
@@ -389,6 +476,9 @@ def avg_map(df):
     avg_map.get_root().html.add_child(folium.Element(legend_html))
     folium_static(avg_map,width=1024,height=600)
 
+# def create_map_based_on_country(df, selected_country):
+#     filtered_df = df[df['country'] == selected_country]
+    
 # load dataset
 df_raw=pd.read_csv('../data/zomato_raw.csv')
 
@@ -454,6 +544,14 @@ st.sidebar.markdown('## Choose your Map')
 options= st.sidebar.selectbox('',('Cuisines', 'Price', 'Aggregate Rating', 'Avg Cost for Two'),index=None,placeholder="Select map...")
 # st.sidebar.write('',option)
 st.sidebar.markdown("""---""")
+
+# countries = df['country'].unique()
+countries = ['World View','United States of America', 'Canada', 'Brazil', 'England','Australia', 'New Zealand', 'Philippines', 'Indonesia',
+    'India', 'Sri Lanka', 'United Arab Emirates', 'Qatar','Turkey', 'Singapore']
+
+st.sidebar.markdown('## Choose a country')
+selected_country = st.sidebar.selectbox('', countries)
+
 
 tab1,tab2=st.tabs(['Management Vision',' '])
 
