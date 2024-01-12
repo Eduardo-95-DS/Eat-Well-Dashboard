af1af75395ea7bdf197b3474072cfcd0682f9e07
Eduardo-95-DS <edu.yori@gmail.com>
2024-01-12 18:28:36 -0300

    city vision added

---
+++ scripts/visao_cidade.py
@@ -0,0 +1,242 @@
+import pandas as pd
+import numpy as np
+import inflection
+import folium
+import streamlit as st
+from folium.plugins import MarkerCluster,MousePosition
+import plotly.express as px
+import plotly.graph_objects as go
+from streamlit_folium import folium_static
+
+# functions=============================================
+COUNTRIES = {
+1: "India",
+14: "Australia",
+30: "Brazil",
+37: "Canada",
+94: "Indonesia",
+148: "New Zeland",
+162: "Philippines",
+166: "Qatar",
+184: "Singapure",
+189: "South Africa",
+191: "Sri Lanka",
+208: "Turkey",
+214: "United Arab Emirates",
+215: "England",
+216: "United States of America"}
+
+COLORS = {
+"3F7E00": "darkgreen",
+"5BA829": "green",
+"9ACD32": "lightgreen",
+"CDD614": "orange",
+"FFBA00": "red",
+"CBCBC8": "darkred",
+"FF7800": "darkred"}
+
+def rename_columns(dataframe):
+    df = dataframe.copy()
+    title = lambda x: inflection.titleize(x)
+    snakecase = lambda x: inflection.underscore(x)
+    spaces = lambda x: x.replace(" ", "")
+    cols_old = list(df.columns)
+    cols_old = list(map(title, cols_old))
+    cols_old = list(map(spaces, cols_old))
+    cols_new = list(map(snakecase, cols_old))
+    df.columns = cols_new
+    return df.columns
+
+def country_name(country_id):
+    return COUNTRIES[country_id]
+
+def create_price_tye(price_range):
+    if price_range == 1:
+        return "cheap"
+    elif price_range == 2:
+        return "normal"
+    elif price_range == 3:
+        return "expensive"
+    else:
+        return "gourmet"
+
+def color_name(color_code):
+    return COLORS[color_code]
+
+def dollarize (df):
+    df['average_cost_for_two']=np.where(df['currency']=='Botswana Pula(P)',   df['average_cost_for_two'] * 0.074, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Brazilian Real(R$)', df['average_cost_for_two'] * 0.21, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Emirati Diram(AED)', df['average_cost_for_two'] * 0.27, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Indian Rupees(Rs.)', df['average_cost_for_two'] * 0.012, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Indonesian Rupiah(IDR)', df['average_cost_for_two'] * 0.000065, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Pounds(£)', df['average_cost_for_two'] * 1.27, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Qatari Rial(QR)', df['average_cost_for_two'] * 0.27, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Rand(R)', df['average_cost_for_two'] * 0.054, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Sri Lankan Rupee(LKR)', df['average_cost_for_two'] * 0.0031, df['average_cost_for_two'] )
+    df['average_cost_for_two']=np.where(df['currency']=='Turkish Lira(TL)', df['average_cost_for_two'] * 0.034, df['average_cost_for_two'] )
+    return df
+
+def cuisines (df,col):
+    df2=df['cuisines'].str.split(',',expand=True)
+    df2= df2.replace(',','', regex=True)
+    df2= df2.replace(' ','', regex=True)
+    df2[col]=df[col]
+    df2.fillna('empty', inplace=True)
+    df2=df2.drop_duplicates(ignore_index=True)
+    
+    df2['Combined'] = df2[0].astype(str) +','+ df2[1] +','+df2[2]+','+df2[3]+','+df2[4]+','+df2[5]+','+df2[6]+','+df2[7] 
+    
+    df2= df2.replace(',empty','', regex=True)
+    df2=df2[['Combined',col]]
+    df2=df2.assign(var1=df2['Combined'].str.split(',')).explode('var1').rename(columns={'var1':'cuisines'})
+    df2=df2.drop(columns=['Combined'])
+    df2=df2.drop_duplicates(ignore_index=True)
+
+    return df2
+
+#==================================================================================================
+
+# load dataset
+df_raw=pd.read_csv('../data/zomato_raw.csv')
+
+# cleaning==============================================
+df_raw=df_raw.drop_duplicates(keep='first')
+
+df_raw=df_raw.dropna(ignore_index=True)
+
+df_raw.columns=rename_columns(df_raw)
+
+list=[]
+for index, row in df_raw.iterrows():
+    codes=country_name(row['country_code'])
+    list.append(codes)
+
+df_raw['country']=list
+len(df_raw['country'].unique())
+
+df_raw['price_range']=df_raw['price_range'].apply(create_price_tye)
+df_raw['price_range'].unique()
+
+list=[]
+for index, row in df_raw.iterrows():
+    codes=color_name(row['rating_color'])
+    list.append(codes)
+
+df_raw['rating_color']=list
+df_raw['rating_color'].unique()
+
+df_raw=dollarize(df_raw)
+
+df_raw=df_raw.drop(columns=['switch_to_order_menu'])
+
+df_raw=df_raw.replace(25000017,25,inplace=False)
+
+# df_raw[df_raw['restaurant_id']==95314]
+df_raw.loc[3981,'latitude']=10.023286
+df_raw.loc[3981,'longitude']=76.311371
+
+# df_raw[df_raw['restaurant_id']==18445965]
+df_raw.loc[6625,'latitude']=-25.7449
+df_raw.loc[6625,'longitude']=28.1878
+
+list=df_raw['cuisines'].str.split(',').apply(len)
+df_raw['cuisines_n°']=list
+df_raw[['cuisines','cuisines_n°']].head(5)
+
+df=df_raw.copy()
+
+# Barra lateral=====================================================================
+st.set_page_config(layout='wide')
+# st.dataframe(df)
+
+# image_path='logo.png'
+# image=Image.open(image_path)
+# st.sidebar.image(image,width=120)
+
+st.header('City Vision')
+st.sidebar.markdown('# Eat Well Company')
+st.sidebar.markdown("""---""")
+
+
+tab1,tab2=st.tabs(['Management Vision',' '])
+
+with tab1:
+    with st.container():
+
+        col1,col2=st.columns(2)
+        with col1:
+            st.markdown("<h4 style='text-align: left;'>Most variety of restaurants</h4>",unsafe_allow_html=True)
+            df1=df.drop_duplicates(subset=['restaurant_name'])
+            df1=(df1[['city','country','restaurant_name']].groupby(['city','country'])
+                                                         .count()
+                                                         .sort_values('restaurant_name',ascending=False)
+                                                         .rename(columns={'restaurant_name':'restaurants'})
+                                                         .reset_index())
+                                                         # .head(10))
+            st.dataframe(df1)
+                
+        with col2:
+            st.markdown("<h4 style='text-align: left;'>Most variety of cuisines</h4>",unsafe_allow_html=True)
+            list=df['cuisines'].str.split(',',expand=True)
+            list= list.replace(',','', regex=True)
+            list= list.replace(' ','', regex=True)
+            list[['city','country']]=df[['city','country']]
+            list.fillna('empty', inplace=True)
+            list=list.drop_duplicates(ignore_index=True)
+            
+            list['Combined'] = list[0].astype(str) +','+ list[1] +','+list[2]+','+list[3]+','+list[4]+','+list[5]+','+list[6]+','+list[7] 
+            
+            list= list.replace(',empty','', regex=True)
+            list=list[['Combined','city','country']]
+            list=list.assign(var1=list['Combined'].str.split(',')).explode('var1')
+            list=list.drop(columns=['Combined'])
+            list=list.drop_duplicates(ignore_index=True)
+            df1=(list[['city','country','var1']].groupby(['city','country'])
+                                                .count()
+                                                .sort_values('var1',ascending=False)
+                                                .rename(columns={'var1':'cuisines'})
+                                                .reset_index())
+            st.dataframe(df1)
+            # st.markdown("""---""")
+
+    with st.container():
+
+        col1,col2=st.columns(2)
+        with col1:
+            st.markdown("<h4 style='text-align:left;margin-top: 0px;'>Most restaurants with ratings above 4.5</h4>",unsafe_allow_html=True)
+            df1=df[df['aggregate_rating']>4.5]
+            df1=(df1[['city','country','restaurant_id']].groupby(['city','country'])
+                                                        .count()
+                                                        .sort_values('restaurant_id',ascending=False)
+                                                        .rename(columns={'restaurant_id':'restaurants'})
+                                                        .reset_index())
+                                                        # .head(10))
+            st.dataframe(df1)
+                
+        with col2:
+            st.markdown("<h4 style='text-align:left;margin-top: 0px;'>Most restaurants with ratings below 3.5</h4>",unsafe_allow_html=True)
+            df1=df[(df['aggregate_rating']<=3.5) & (df['votes']>3)]
+            df1=(df1[['city','country','restaurant_id']].groupby(['city','country'])
+                                                        .count()
+                                                        .sort_values('restaurant_id',ascending=False)
+                                                        .rename(columns={'restaurant_id':'restaurants'})
+                                                        .reset_index())
+                                                        # .head(10))
+            st.dataframe(df1)
+            # st.markdown("""---""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
