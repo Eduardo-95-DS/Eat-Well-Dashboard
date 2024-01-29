e058cc2549a4c18d28dfb0eccf6793bd3a91892f
Eduardo-95-DS <edu.yori@gmail.com>
2024-01-29 18:33:18 -0300

    home update

--- deploy-scripts/Home.py
+++ deploy-scripts/Home.py
@@ -18,7 +18,8 @@ st.write('# Eat Well Company Dashboard')
 
 st.markdown("""
             Dashboard built to accompany the company's main metrics     
-            # How to use it?    
+            
+            ##How to use it?    
             
             - Overview:
             	- General metrics and maps for a geographical perspective
